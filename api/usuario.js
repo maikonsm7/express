e1b7225e4a86f2ddf7edbbe5195ed6850ffbfe7d
Maikon Monteiro <maikonsm7@gmail.com>
2024-05-25 20:58:49 -0400

    update bodyParser

---
+++ api/usuario.js
@@ -0,0 +1,8 @@
+function salvar(req, res){
+    res.send('Usuário -> Salvar')
+}
+function obter(req, res){
+    res.send('Usuário -> Obter')
+}
+
+module.exports = {salvar, obter}
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const db = require('./api/cliente')
+const cliente = require('./api/cliente')
+const usuario = require('./api/usuario')
 const app = express()
 const port = 3000
 
 // app.use(bodyParser.text())
 // app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true})) // para envio de formulário do front
 
+// Cliente
 app.get('/clientes', (req, res) => {
-    res.send(db.listarClientes())
+    res.send(cliente.listarClientes())
 })
 
 app.get('/clientes/:id', (req, res) => {
-    res.send(db.buscarCliente(req.params.id))
+    res.send(cliente.buscarCliente(req.params.id))
 })
 
 app.post('/clientes', (req, res) => {
@@ -21,8 +23,12 @@ app.post('/clientes', (req, res) => {
     if(req.body.id){clie.id = req.body.id}
     clie.nome = req.body.nome
     clie.idade = req.body.idade
-    res.send(db.salvarCliente(clie))
+    res.send(cliente.salvarCliente(clie))
 })
+
+// Usuário - padrão mais reduzido. Função que retorna um middleware
+app.post('/usuarios', usuario.salvar)
+app.get('/usuarios', usuario.obter)
 
 app.listen(port, '0.0.0.0', ()=>{
     console.log(`Servidor online na porta ${port}, http://localhost:${port}`)
